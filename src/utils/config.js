e191081b0a207a26ee80496cacca8e9e02f1f001
@Douala237 <yvangodimomo@gmail.com>
2025-07-17 04:40:47 +0100

    update error

---
+++ src/utils/config.js
@@ -0,0 +1,35 @@
+export const SITE_CONFIG = {
+  name: 'Radio Flambeau-Banka',
+  tagline: 'Votre radio communautaire à Banka, Cameroun',
+  description: "Radio communautaire locale diffusant sur 91.5 MHz à Banka, Cameroun. Actualités, musique africaine et émissions communautaires.",
+  url: 'https://www.radioflambeaubanka.com',
+  logo: 'https://www.radioflambeaubanka.com/images/logo.png',
+  image: 'https://www.radioflambeaubanka.com/images/og-image.jpg',
+  foundingDate: '2020',
+  address: {
+    locality: 'Banka',
+    region: 'Haut-Nkam',
+    country: 'CM',
+    postalCode: '00000',
+  },
+  geo: {
+    latitude: 5.2833,
+    longitude: 10.2833,
+  },
+  contact: {
+    phone: '+237-XXX-XXX-XXX',
+    email: 'contact@radioflambeaubanka.com',
+    language: 'French',
+  },
+  social: {
+    facebook: 'https://facebook.com/radioflambeaubanka',
+    twitter: 'https://twitter.com/radioflambeaubanka',
+    instagram: 'https://instagram.com/radioflambeaubanka',
+    youtube: 'https://youtube.com/@radioflambeaubanka',
+  },
+  broadcast: {
+    displayName: 'Radio Flambeau-Banka',
+    frequency: '91.5 MHz',
+    timezone: 'Africa/Douala',
+  },
+};
